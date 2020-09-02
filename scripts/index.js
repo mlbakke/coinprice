47e2a585578179f477aed422414a6edad7a67a5b
mlbakke <Martin.Bakke@student.uib.no>
2020-09-02 12:08:00 +0200

    added bold class to coin.name

--- scripts/index.js
+++ scripts/index.js
@@ -10,22 +10,23 @@ function printCoins(coins) {
     for (let coin of coins) {
         const row = document.createElement("tr");
         const rank = document.createElement("td");
-        const rankT = document.createTextNode(coin.market_data.market_cap_rank)
+        const rankT = document.createTextNode(coin.market_data.market_cap_rank);
         const name = document.createElement("td");
-        const nameT = document.createTextNode(coin.name)
+        const nameT = document.createTextNode(coin.name);
         const cap = document.createElement("td");
-        const capT = document.createTextNode(coin.market_data.market_cap.usd)
+        const capT = document.createTextNode(coin.market_data.market_cap.usd);
         const price = document.createElement("td");
-        const priceT = document.createTextNode(coin.market_data.current_price.usd)
+        const priceT = document.createTextNode(coin.market_data.current_price.usd);
         const vol = document.createElement("td");
-        const volT = document.createTextNode(coin.market_data.total_volume.usd)
+        const volT = document.createTextNode(coin.market_data.total_volume.usd);
         const supply = document.createElement("td");
-        const supplyT = document.createTextNode(coin.market_data.circulating_supply)
+        const supplyT = document.createTextNode(coin.market_data.circulating_supply);
         const change = document.createElement("td");
-        const changeT = document.createTextNode(coin.market_data.price_change_percentage_24h_in_currency.usd)
+        const changeT = document.createTextNode(coin.market_data.price_change_percentage_24h_in_currency.usd);
         const tds = [rank, name, cap, price, vol, supply, change];
 
-        row.classList.add('table-row')
+        row.classList.add('table-row');
+        name.classList.add('table-bold');
 
         rank.appendChild(rankT);
         name.appendChild(nameT);

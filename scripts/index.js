4fa52fab14f207237c21af45b11948a0679ca376
mlbakke <Martin.Bakke@student.uib.no>
2020-10-01 18:34:56 +0200

    done: table sorting
    user can now click on any th to sort the table

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,15 @@
 const table = document.querySelector('.table-body');
 const currencySign = '$';
-const currency = 'usd'
+const currency = 'usd';
+const perPage = '50';
 let page = 1;
 
 async function getCoins() {
     //fetch coinlist
-    const coinList = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_rank_desc&per_page=100&page=${page}&sparkline=false&price_change_percentage=24h`)
+    const coinList = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_rank_desc&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=24h`)
         .catch(err => {
             if (err.response.status === 404) {
-                return 0;
+                return null;
             }
             throw err;
         }
@@ -98,4 +99,83 @@ const loadBtn = document.querySelector('.btn-load');
 loadBtn.addEventListener('click', () => {
     page++;
     getCoins();
+    // Sort table after rank when loading more
+    sortTable(0);
 });
+
+// SORTING TABLE
+const ths = document.querySelectorAll('th');
+// Sort table on th click
+ths.forEach((th) => {
+    th.addEventListener('click', () => {
+        sortTable(th.dataset.key);
+    });
+});
+
+function sortTable(n) {
+    let rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    switching = true;
+    dir = "asc";
+    // Loop until no switching has been done
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        // Loop through all table rows
+        for (i = 0; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            // Compare current row next
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+
+            // If rows are name or ticker:
+            if (n == 1 || n == 2) {
+                // Check if the rows should switch place
+                if (dir === "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        // If so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if (dir === "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        // If so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            } else {
+                // For rows with numbers, create floats without currency sign and commas
+                const xNum = parseFloat(x.innerHTML.replace(/[Ξ฿¥€£$,]/g, ""));
+                const yNum = parseFloat(y.innerHTML.replace(/[Ξ฿¥€£$,]/g, ""));
+                if (dir === "asc") {
+                    if (xNum > yNum) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if (dir === "desc") {
+                    if (xNum < yNum) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase switchCount by 1
+            switchcount ++;
+        } else {
+            /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}

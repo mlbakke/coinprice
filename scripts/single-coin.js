45ed1216a8004a6ceb52a482995d4ff5d64cce4c
Martin Bakke <bakkemartin11@gmail.com>
2020-11-14 17:30:34 +0100

    get+print SingleCoin functions added
    coinId to be made dynamic
    
    print function to be further fleshed out

--- scripts/single-coin.js
+++ scripts/single-coin.js
@@ -4,7 +4,7 @@ async function getSingleCoin() {
 	//fetch coin information
 	const coin = await axios
 		.get(
-			`https://api.coingecko.com/api/v3/coins/${coinId}`
+			`https://api.coingecko.com/api/v3/coins/${coinId}?vs_currency=${currency}`
 		)
 		.catch((err) => {
 			if (err.response.status === 404) {
@@ -13,8 +13,19 @@ async function getSingleCoin() {
 			throw err;
 		});
 	//print coin data
-	console.log(coin.data);
+    console.log(coin.data);
+    printSingleCoin(coin.data);
 }
 
-function printSingleCoin(coin) {}
+function printSingleCoin(coin) {
+    const heading = document.querySelector('.coin-stats__heading');
+    heading.innerHTML = `<div class=coin-stats__heading--left>
+                            <img src="${coin.image.small}" alt="${coin.name} logo" class="coin-stats__heading--logo"></img>
+                            <h4 class="coin-stats__heading--name">${coin.name}</h4>
+                            <h4 class="coin-stats__heading--ticker">(${coin.symbol.toUpperCase()})</h4>
+                        </div>
+                        <div class=coin-stats__heading--right>
+                            ${currencySign}${separateThousands(coin.market_data.current_price[currency])}
+                        </div>`;
+}
 getSingleCoin();

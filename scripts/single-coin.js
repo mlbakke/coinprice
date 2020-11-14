032118ff22d98e0845793f1628ceedd7df0e1617
Martin Bakke <bakkemartin11@gmail.com>
2020-11-14 20:20:53 +0100

    prints price changes, coin info and description
    to do: fix overflow/scrolling for description

--- scripts/single-coin.js
+++ scripts/single-coin.js
@@ -28,13 +28,49 @@ function printSingleCoin(coin) {
                             ${currencySign}${separateThousands(coin.market_data.current_price[currency])}
                         </div>`;
 
-    const left = document.querySelector('.coin-stats__info--left');
-    left.innerHTML = `
-        <div class="coin-stats__info--links">Website: <a href="${coin.links.homepage[0]}" class="coin-stats__info--link">${coin.links.homepage[0]}</a></div>
-        <div class="coin-stats__info--links">Explorer: <a href="${coin.links.blockchain_site[0]}" class="coin-stats__info--link">${coin.links.blockchain_site[0]}</a></div>
-        <div class="coin-stats__info--links">Source code: <a href="${coin.links.blockchain_site[0]}" class="coin-stats__info--link">${coin.links.repos_url.github[0]}</a></div>
-        <div class="coin-stats__info--links">Forum: <a href="${coin.links.blockchain_site[0]}" class="coin-stats__info--link">${coin.links.official_forum_url[0]}</a></div>
-        <div class="coin-stats__info--links">Subreddit: <a href="${coin.links.blockchain_site[0]}" class="coin-stats__info--link">${coin.links.subreddit_url}</a></div>
+    const links = document.querySelector('.coin-stats__info--links');
+    links.innerHTML = `
+        <a href="${coin.links.homepage[0]}" class="coin-stats__info--link">Website</a>
+        <a href="${coin.links.blockchain_site[0]}" class="coin-stats__info--link">Explorer</a>
+        <a href="${coin.links.blockchain_site[0]}" class="coin-stats__info--link">Source code</a>
+        <a href="${coin.links.blockchain_site[0]}" class="coin-stats__info--link">Forum</a>
+        <a href="${coin.links.blockchain_site[0]}" class="coin-stats__info--link">Subreddit</a>
+    `
+    const priceChange = document.querySelector('.coin-stats__info--price-change');
+    priceChange.innerHTML = `
+            <h5 class="heading__quinary">Price Changes<h5>
+            <div class="coin-stats__info--change-overview">
+                <div>1h: <span class="coin-change">${toDecimals(coin.market_data.price_change_percentage_1h_in_currency[currency], 2)}%</span></div>
+                <div>1d: <span class="coin-change">${toDecimals(coin.market_data.price_change_percentage_24h_in_currency[currency], 2)}%</span></div>
+                <div>1w: <span class="coin-change">${toDecimals(coin.market_data.price_change_percentage_7d_in_currency[currency], 2)}%</span></div>
+                <div>1m: <span class="coin-change">${toDecimals(coin.market_data.price_change_percentage_30d_in_currency[currency], 2)}%</span></div>
+                <div>1y: <span class="coin-change">${toDecimals(coin.market_data.price_change_percentage_1y_in_currency[currency], 2)}%</span></div>
+            </div>
+    `;
+    // Color change for positive/negative price change
+    for (coinChange of document.querySelectorAll('.coin-change')) {
+        isPositive(coinChange);
+    }
+
+    const divLeft = document.querySelector('.coin-stats__info--div-left');
+    divLeft.innerHTML = `
+            <h5 class="heading__quinary">Rank</h5> <span>${coin.market_data.market_cap_rank}</span>
+            <h5 class="heading__quinary">24h Volume</h5> <span>${currencySign}${separateThousands(coin.market_data.total_volume[currency])}</span>
+            <h5 class="heading__quinary">ATH</h5> <span>${currencySign}${separateThousands(coin.market_data.ath[currency])}</span>
+    `
+
+    const divRight = document.querySelector('.coin-stats__info--div-right');
+    divRight.innerHTML = `
+            <h5 class="heading__quinary">Market Cap</h5> <span>${currencySign}${separateThousands(coin.market_data.market_cap[currency])}</span>
+            <h5 class="heading__quinary">24h low / 24h high</h5> <span>${currencySign}${separateThousands(coin.market_data.low_24h[currency])} / ${currencySign}${separateThousands(coin.market_data.high_24h[currency])}</span>
+            <h5 class="heading__quinary">ROI</h5> <span id="ROI">${coin.market_data.current_price[currency]}</span>
+    `
+    isPositive(document.querySelector('#ROI'));
+
+    const description = document.querySelector('.coin-stats__info--description');
+    description.innerHTML = `
+            <summary><h5 class="heading__quinary">Description</h5></summary>
+                ${coin.description.en}
     `
 }
 getSingleCoin();
